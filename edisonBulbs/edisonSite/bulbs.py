52d3565b55aa7221ef1cfc787e3bc6e5a8729c29
Alexander Grbach <alex@grbach.com>
2017-07-10 20:10:35 -0400

    added bulb status, ui now reflects bulb status

--- edisonBulbs/edisonSite/bulbs.py
+++ edisonBulbs/edisonSite/bulbs.py
@@ -6,6 +6,7 @@ import threading
 import time
 import edisonBulbs as bulbs
 from sys import exit
+from flask import json
 
 try:
     from flask import Flask, render_template
@@ -18,26 +19,37 @@ app = Flask(__name__)
 
 chan_list = list(bulbs.chan_list)
 
+@app.route('/status/')
+def status():
+	data = []
+	for chan in chan_list:
+		data.append(bulbs.GPIO.input(chan))
+	response = app.response_class(
+	    response=json.dumps(data),
+	    status=200,
+	    mimetype='application/json'
+	)
+	return response
 
 @app.route('/')
 def home():
 	return render_template('index.html')
 
-@app.route('/all/off')
+@app.route('/all/off/')
 def alloff():
 	bulbs.GPIO.output(chan_list, bulbs.GPIO.LOW)
-	return "ok"
+	return status()
 
 @app.route('/all/on/')
 def allon():
 	bulbs.GPIO.output(chan_list, bulbs.GPIO.HIGH)
-	return "ok"
+	return status()
 
 @app.route('/toggle/<bulb>')
 def toggle(bulb):
 	bulbs.toggle_bulb(chan_list[int(bulb)])
 	# return "ok"
-	return "ok"
+	return status()
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
--- edisonBulbs/edisonSite/static/main.js
+++ edisonBulbs/edisonSite/static/main.js
@@ -1,1 +1,30 @@
+$( document ).ready(function() {
+    $.get('/status/')
+	.done(function(data){bulbStatus(data)})
+});
 
+function toggleBulb(bulb){
+	$.get('/toggle/' + bulb)
+	.done(function(data){bulbStatus(data)})
+}
+
+function allOn(){
+	$.get('/all/on/')
+	.done(function(data){bulbStatus(data)})
+}
+
+function allOff(){
+	$.get('/all/off/')
+	.done(function(data){bulbStatus(data)})
+}
+
+function bulbStatus(bulbs){
+	console.log(bulbs)
+	bulbs.forEach(setLightCss)
+}
+
+function setLightCss(element, index, array){
+	var light = $('.fa-lightbulb-o').eq(index)
+	if (element == 1 && !light.hasClass("light_on")){light.addClass("light_on")}
+	else if (element == 0 && light.hasClass("light_on")){light.removeClass("light_on")}
+}
