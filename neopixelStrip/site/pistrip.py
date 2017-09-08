398d1e5d169974b76eeb305506053c83f4f83389
Alexander Grbach <alex@grbach.com>
2017-09-08 05:39:59 +0000

    Needed to commit nonworking code

--- neopixelStrip/site/pistrip.py
+++ neopixelStrip/site/pistrip.py
@@ -4,44 +4,44 @@ from random import randint
 from neopixel import *
 
 # LED strip configuration:
-LED_COUNT      = 299     # Number of LED pixels.
-LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
-LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
-LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
-LED_CHANNEL    = 0       # PWM channel
-LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_COUNT	  = 299	 # Number of LED pixels.
+LED_PIN		= 18	  # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ	= 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA		= 5	   # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 255	 # Set to 0 for darkest and 255 for brightest
+LED_CHANNEL	= 0	   # PWM channel
+LED_INVERT	 = False   # True to invert the signal (when using NPN transistor level shift)
 
 class LedColors:
-    def __init__(self):
-        self.colors = {
-            'Red': Color(0, 255, 0),
-            'Yellow': Color(255,255,0),
-            'Green': Color(255, 0, 0),
-            'Cyan': Color(2550,0,255),
-            'Blue': Color(0,0,255),
-            'Magenta': Color(0,255,255),
-            'White': Color(255,255,255),
-            'Black': Color(0, 0, 0)
-            }
+	def __init__(self):
+		self.colors = {
+			'Red': Color(0, 255, 0),
+			'Yellow': Color(255,255,0),
+			'Green': Color(255, 0, 0),
+			'Cyan': Color(2550,0,255),
+			'Blue': Color(0,0,255),
+			'Magenta': Color(0,255,255),
+			'White': Color(255,255,255),
+			'Black': Color(0, 0, 0)
+			}
 
-        self.color_index = 0
+		self.color_index = 0
 
-    def get(self, key):
-        return self.colors.get(key)
+	def get(self, key):
+		return self.colors.get(key)
 
-    def cycle(self):
-        color = self.colors.values()[self.color_index]
-        self.color_index += 1
-        if(self.color_index >= len(self.colors.keys())):
-            self.color_index = 0
-        return color
+	def cycle(self):
+		color = self.colors.values()[self.color_index]
+		self.color_index += 1
+		if(self.color_index >= len(self.colors.keys())):
+			self.color_index = 0
+		return color
 
-    def random(self):
-        return Color(self.__randint_for_color(), self.__randint_for_color(), self.__randint_for_color())
+	def random(self):
+		return Color(self.__randint_for_color(), self.__randint_for_color(), self.__randint_for_color())
 
-    def __randint_for_color(self):
-        return randint(0, 255)/randint(1,10)
+	def __randint_for_color(self):
+		return randint(0, 255)/randint(1,10)
 
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
 strip.begin()
@@ -49,59 +49,62 @@ strip.begin()
 led_colors = LedColors() 
 
 def brightness(b=60):
-    if 0 <= b <= 255:
-        strip.setBrightness(b)
-    else:
-        raise ValueError('Brightness must be between 0 and 255')
+	if 0 <= b <= 255:
+		strip.setBrightness(b)
+	else:
+		raise ValueError('Brightness must be between 0 and 255')
 
 def get_brightness():
-    return strip.getBrightness()
+	return strip.getBrightness()
 
 def clear():
-    """Clear the buffer"""
-    for x in range(length()):
-        set_pixel(x, led_colors.get("Black"))
+	"""Clear the buffer"""
+	for x in range(length()):
+		set_pixel(x, led_colors.get("Black"))
 
 def off():
-    clear()
-    show()
+	clear()
+	show()
 
 def set_pixel(x=0, color=led_colors.get('White')):
-    if x is not None:
-        strip.setPixelColor(x, color)
+	if x is not None:
+		strip.setPixelColor(x, color)
+
+def set_pixel_range(start=0, end=299, color=led_colors.get("White")):
+	if start < end:
+		for x in range(start, end, 1):
+			strip.setPixelColor(x, color)
+	elif start > end:
+		for x in range(end, start, -1):
+			strip.setPixelColor(x, color)
+	else:
+		strip.set_pixel(start, color)
 
 def set_all_pixels(color=led_colors.get("White")):
-    for x in range(length()):
-        strip.setPixelColor(x, color)
+	for x in range(length()):
+		strip.setPixelColor(x, color)
 
 def get_pixel(pixel):
-    if 0 < pixel <= length(): 
-        return strip.getPixelColor(pixel)
+	if 0 < pixel <= length(): 
+		return strip.getPixelColor(pixel)
 
 def get_pixels():
-    return [get_pixel(x) for x in range(length()+1)]
+	return [get_pixel(x) for x in range(length()+1)]
 
 def show():
-    strip.show()
+	strip.show()
  
 def length():
-    return strip.numPixels()
+	return strip.numPixels()
 
 def ColorToRGB(color):
-	print(color)
 	green = color >> 16
-	print(color)
-    red = color >> 16 & color >> 8 
-	print(color)
-    blue = color >> 16 & color >> 8 & color
-	print(color)
-
-
-    print(color,red,green,blue)
-    return (red,green,blue)
+	red = ~(~(color >> 8) | ~(color >> 16))
+	blue = ~(~(color >> 16) | ~(color))
+	return [color,red,green,blue]
 
 def _clean_shutdown():
-    off()
+	off()
 
 off()
 atexit.register(_clean_shutdown)
--- neopixelStrip/site/site.py
+++ neopixelStrip/site/site.py
@@ -33,16 +33,22 @@ class perpetualTimer():
 def default():
     pass
 
+##############################################################################
+##                                                                          ##
+##                                 WebStuff                                 ##
+##                                                                          ##
+##############################################################################
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
 	return render_template('index.html')
 
-@app.route('/pixel/get/<id>')
-def getPixelColor(id):
+@app.route('/pixel/get/<pixel>')
+def get_pixel_color(pixel):
 	data = []
-	data.append(strip.ColorToRGB(strip.get_pixel(int(id))))
+	data.append(strip.ColorToRGB(strip.get_pixel(int(pixel))))
 	response = app.response_class(
 	    response=json.dumps(data),
 	    status=200,
@@ -51,7 +57,7 @@ def getPixelColor(id):
 	return response
 
 @app.route('/pixels/get/all')
-def getPixelColors():
+def get_pixel_colors():
 	data = []
 	for x in range(1,strip.length()):
 		data.append(strip.ColorToRGB(strip.get_pixel(x)))
@@ -63,20 +69,113 @@ def getPixelColors():
 	)
 	return response
 
-@app.route('/pixels/set/all/<r>/<g>/<b>')
-def setPixelColors(r,g,b):
+@app.route('/set/pixel/all/<r>/<g>/<b>')
+def set_pixel_all(r,g,b):
 	strip.set_all_pixels(strip.Color(int(g),int(r),int(b)))
 	strip.show()
 
-	return getPixelColors()
+	return get_pixel_colors()
 
-@app.route('/pixel/set/<id>/<r>/<g>/<b>')
-def setPixelColor(id,r,g,b):
-	# we use bgr in this house
-	strip.set_pixel(int(id), strip.Color(int(g),int(r),int(b)))
+@app.route('/set/pixel/range/<start_pixel>/<end_pixel>/<r>/<g>/<b>/')
+def set_pixel_range(start_pixel,end_pixel,r,g,b):
+	try:
+		start = validate_int(start_pixel, 0, 299, "start")
+		end = validate_int(end_pixel, 0, 299, "end")
+		color = validate_color(r,g,b)
+	except ValueError as e:
+		return response("error", e.args)
+
+	strip.set_pixel_range(start, end, color)
 	strip.show()
-	return getPixelColors()
-	# return getPixelColor(id)
+	return get_pixel_colors()
+
+@app.route('/set/pixel/<pixel>/<r>/<g>/<b>')
+def set_pixel(pixel,r,g,b):
+	# we use bgr in this house
+	strip.set_pixel(int(pixel), strip.Color(int(g),int(r),int(b)))
+	strip.show()
+	return get_pixel_colors()
+
+##############################################################################
+##                                                                          ##
+##                             Response Helpers                             ##
+##                                                                          ##
+##############################################################################
+
+def response(response_type, data_object):
+	data = {"app": "led_strip",
+			"numberOfBulbs": strip.length(),
+			}
+	status_code = -1
+
+	if response_type = "error":
+		status_code = 400
+		data.update({"Error" : {"Type" : "general", "Message" : data_object}})
+	elif response_type = "validation_error"
+		status_code = 400
+		data.update({"Error" : {"Type" : "validation", "Message" : data_object}})
+	else
+		status_code = 200
+
+
+	response = app.response_class(
+	    response=json.dumps(data),
+	    status=status_code,
+	    mimetype='application/json'
+	)
+	return response
+
+class response:
+	"""docstring for response"""
+	def __init__(self, arg):
+		super(response, self).__init__()
+
+		self.app_name = "neopixel_stirp"
+		self.length = strip.length()
+		self.status = ""
+		self.status_code = 
+
+
+		
+
+##############################################################################
+##                                                                          ##
+##                                Validation                                ##
+##                                                                          ##
+##############################################################################
+
+def validate_color(r,g,b):
+	try:
+		r = validate_int(r, 0, 255, "r")
+		g = validate_int(g, 0, 255, "g")
+		b = validate_int(b, 0, 255, "b")
+	except ValueError:
+		raise 
+
+	return strip.Color(g,r,b)
+
+def validate_int(input_int, minimum, maximum, friendly_name):
+	try:
+		valid_input = int(input_int)
+	except ValueError:
+		raise ValueError(friendly_name + 
+						" must be a whole number"
+						)
+
+	if minimum < valid_input < maximum:
+		raise ValueError(friendly_name + 
+						' must be between ' + 
+						minimum + 'and' + maximum
+						)
+
+	return valid_input
+
+
+##############################################################################
+##                                                                          ##
+##                                App Start                                 ##
+##                                                                          ##
+##############################################################################
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
